3e21772e23773a8d3cd7edae94fbb21fde41145b
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2022-12-17 16:02:03 -0700

    Moved functions to TS

---
+++ src/functions.ts
@@ -0,0 +1,36 @@
+
+
+
+
+//  ███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
+//  ██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
+//  █████╗  ██║   ██║██╔██╗ ██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
+//  ██╔══╝  ██║   ██║██║╚██╗██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
+//  ██║     ╚██████╔╝██║ ╚████║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
+//  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝ 
+
+//⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻//
+
+
+import { get } from 'http';
+import { existsSync, mkdirSync, createWriteStream } from "fs";
+
+// DOWNLOAD LOG FILE
+function download(url: string, id: string){
+    if (!existsSync("./logs")) {
+        mkdirSync("./logs");
+    }
+    get(url, (result) => {
+        result.on("error", (err) => console.error(err));
+        result.pipe(createWriteStream("./logs/" + id + ".txt"));
+    });
+    console.log(`Processing a log file`);
+}
+
+//⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻//
+
+export default {
+    download,
+}
+
+//⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻//

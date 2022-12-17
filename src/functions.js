aa5ef947c939bc36a71a4b1694502ae117242fc6
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2022-12-17 15:27:07 -0700

    Changed indentation on functions for consistency

--- src/functions.js
+++ src/functions.js
@@ -11,25 +11,25 @@
 
 //⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻//
 
-        let http = require('http');
-        let fs = require(`fs`);
+const http = require('http');
+const fs = require(`fs`);
 
-        // DOWNLOAD LOG FILE
-        function download(url, id){
-            if (!fs.existsSync("./logs")) {
-                fs.mkdirSync("./logs");
-            }
-            http.get(url, (result) => {
-                result.on("error", (err) => console.error(err));
-                result.pipe(fs.createWriteStream("./logs/" + id + ".txt"));
-            });
-            console.log(`Processing a log file`);
-        }
-        
+// DOWNLOAD LOG FILE
+function download(url, id){
+    if (!fs.existsSync("./logs")) {
+        fs.mkdirSync("./logs");
+    }
+    http.get(url, (result) => {
+        result.on("error", (err) => console.error(err));
+        result.pipe(fs.createWriteStream("./logs/" + id + ".txt"));
+    });
+    console.log(`Processing a log file`);
+}
+
 //⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻//
 
-        module.exports = {
-            download,
-        }
+module.exports = {
+    download,
+}
 
 //⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻//
